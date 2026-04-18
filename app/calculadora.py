ceec43994b1f3e057009ac8c444e93b2dc77d6e6
Juadagu <juadagu@gmail.com>
2026-04-18 08:17:25 -0500

    v0.1.1 Aplicando Black

--- app/calculadora.py
+++ app/calculadora.py
@@ -2,14 +2,18 @@
 
 AUTORES = "jsocampod@eafit.edu.co, asrodriguo@eafit.edu.co, jdgutierrg@eafit.edu.co, mgutierrej@eafit.edu.co"  # IMPORTANTE: Reemplaza con los usuarios de correo de EAFIT de los estudiantes que participaron en la entrega, separados por comas.
 
+
 def sumar(a, b):
     return a + b
+
 
 def restar(a, b):
     return a - b
 
+
 def multiplicar(a, b):
     return a * b
+
 
 def dividir(a, b):
     if b == 0:
